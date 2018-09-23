7ffcfa6076cf0a55bbe56b7bdcd1e36a72dc39a8
Ryoung27 <RRYoung89@gmail.com>
2018-09-23 13:26:03 -0500

    bulls eye

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,20 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app", preferredStyle: .alert)
+        
+        let message = "The value of the slider is now: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message , preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
-
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
     
     
 
